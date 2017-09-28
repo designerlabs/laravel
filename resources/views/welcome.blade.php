90b3c815d45025b1db3a797844213b5fa142932b
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2017-09-28 18:30:10 +0800

    initial laravel commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row col-md-12">
+        <h1>Hello world</h1>
+        <h1>sdfsf</h1>
+        <h2>{{ "hello" }}</h2>
+    </div>
+    
+@endsection
